5dd8d3094ba4cb8d9e24815024a81a5fa1665785
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2024-12-19 18:55:01 -0600

    menus

--- index.php
+++ index.php
@@ -4,26 +4,35 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gabriel Estrella</title>
+  <title>Bienvenido</title>
   <link rel="stylesheet" href="styles/main.css">
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+  <script src="lib/responsiveslides.min.js"></script>
 </head>
 
 <body>
-  <Header>
-    <div class="div_header ancho">
-      <img src="img/logotransparente.png  " alt="">
-      <h1>REGISTROS RESIDENCIA</h1>
-    </div>
-  </Header>
-  <nav>
-    <ul>
-      <li><a href="#">texto 1</a></li>
-      <li><a href="#">texto 2</a></li>
-      <li><a href="#">texto 3</a></li>
+
+  <?php
+  include "header_menu.php"
+  ?>
+
+  <section class="section_slides">
+    <ul class="rslides">
+      <li><img src="img/tec1.jpg" alt=""></li>
+      <li><img src="img/tec2.jfif" alt=""></li>
+      <li><img src="img/tec3.jpg" alt=""></li>
     </ul>
-  </nav> 
+  </section>
 
+  <?php
+  include "footer.php"
+  ?>
 
+  <script>
+    $(function() {
+      $(".rslides").responsiveSlides();
+    });
+  </script>
 
 </body>
 
---
+++ menu_registro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registro</title>
+  <link rel="stylesheet" href="styles/main.css">
+
+</head>
+
+<body>
+
+  <?php
+  include "header_menu.php"
+  ?>
+
+  <?php
+  include "footer.php"
+  ?>
+
+</body>
+
+</html>
