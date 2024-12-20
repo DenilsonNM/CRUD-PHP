a040d0b021dc3fbf7b94e0e81d4aee3f37ec49f3
Denilson Jesus Nuñez Morales <denilsonjnm31@gmail.com>
2024-12-19 18:02:47 -0600

    push repo

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gabriel Estrella</title>
+  <link rel="stylesheet" href="styles/main.css">
+</head>
+
+<body>
+  <Header>
+    <div class="div_header ancho">
+      <img src="img/logotransparente.png  " alt="">
+      <h1>REGISTROS RESIDENCIA</h1>
+    </div>
+  </Header>
+  <nav>
+    <ul>
+      <li><a href="#">texto 1</a></li>
+      <li><a href="#">texto 2</a></li>
+      <li><a href="#">texto 3</a></li>
+    </ul>
+  </nav> 
+
+
+
+</body>
+
+</html>
